5147c2755d12e3ab33164b3b1e4fb88c98227467
Corentin Henry <corentinhenry@gmail.com>
2019-06-22 21:34:01 +0200

    use ring-http-response in handler

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.6.3"]]
+                 [ring "1.6.3"]
+                 [metosin/ring-http-response "0.9.0"]]
   :repl-options {:init-ns ring-app.core}
   :plugins [[cider/cider-nrepl "0.22.0-beta4"]]
   :main ring-app.core)
