5c518346354261bb11f3a8aff8b5ddf009c620aa
Corentin Henry <corentinhenry@gmail.com>
2019-06-22 20:36:38 +0200

    use ring's response function

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,12 +1,12 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
 
 (defn handler [request-map]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> your IP is: "
-              (:remote-addr request-map)
-              "</body></html>")})
+  (response/response
+   (str "<html><body> your IP is: "
+        (:remote-addr request-map)
+        "</body></html>")))
 
 (defn -main []
   (jetty/run-jetty
