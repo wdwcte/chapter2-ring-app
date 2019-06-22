3e24cc70a4c82d2e89ce7fa7ed0148faef93812b
Corentin Henry <corentinhenry@gmail.com>
2019-06-22 20:28:43 +0200

    add first handler

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,6 +1,8 @@
 (ns ring-app.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [request-map]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<html><body> your IP is: "
+              (:remote-addr request-map)
+              "</body></html>")})
