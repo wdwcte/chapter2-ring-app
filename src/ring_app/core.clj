3938f4abee8053887d911b8574c422c369d4b9d8
Corentin Henry <corentinhenry@gmail.com>
2019-06-22 20:54:26 +0200

    add middleware

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -8,8 +8,22 @@
         (:remote-addr request-map)
         "</body></html>")))
 
+(defn wrap-nocache [handler]
+  ;; we return a handler. A handler takes a request, modifies it, and
+  ;; return it
+  (fn [request]
+    (-> request
+        handler ;; we call the previous handler
+        (assoc-in [:headers "Pragma"] "no-cache") ;; we do our own
+                                                  ;; modifications to
+                                                  ;; whatever the
+                                                  ;; previous handler
+                                                  ;; returned
+        )))
+
 (defn -main []
   (jetty/run-jetty
-   handler
+   (-> handler
+       wrap-nocache) ;; create our handler from our middlewares
    {:port 3000
     :join? false}))
