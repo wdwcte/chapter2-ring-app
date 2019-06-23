fd0d4516d0a9b7da808c0501940904258d599083
Corentin Henry <corentinhenry@gmail.com>
2019-06-23 10:26:31 +0200

    add default routes

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -39,8 +39,16 @@
    ["/hello/:value" {:get hello-handler}]])
 
 (def handler
-  (reitit/ring-handler
-   (reitit/router routes)))
+  (reitit/routes
+   (reitit/ring-handler
+    (reitit/router routes))
+   (reitit/create-default-handler
+    {:not-found
+     (constantly (response/not-found "404 - Page not found"))
+     :method-not-allowed
+     (constantly (response/method-not-allowed "405 - Not allowed"))
+     :not-acceptable
+     (constantly (response/not-acceptable "406 - Not acceptable"))})))
 
 (defn wrap-nocache [handler]
   ;; we return a handler. A handler takes a request, modifies it, and
